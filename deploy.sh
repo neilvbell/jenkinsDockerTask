14ab989bde7fb81efefcbd2d8ad9df315b31b847
neilvbell <neilvbell@yahoo.co.uk>
2025-07-18 10:42:38 +0100

    Add files via upload
    adding deply script but will change this to a Jenkinsfile later

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# clean-up by deleting all current containers prior to running fresh each time
+	docker rm -f $(docker ps -a)
+# Create a Network
+	docker network create my-network
+#Create a volume
+
+#Build flask-app and mysql images
+	cd ~/dockerfiles2/dockerfileexercise/Task2/db/
+	docker build -t mysql_img .
+	cd ~/dockerfiles2/dockerfileexercise/Task2/flask-app/
+	docker build -t flask_new_img .
+
+#Run mysql container
+	docker run -dit --network my-network --name mysql1 mysql_img
+#Run flask-app container
+	docker run -dit --network my-network --name flask-app flask_new_img
+
+#Run nginx from official image (but need nginx.conf edited)
+	cd ~/dockerfiles2/dockerfileexercise/Task2/nginx/
+	docker run -dit -p 80:80 --network my-network -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf --name nginx2 nginx
+#Show all containers
+	docker ps
+exit 0
